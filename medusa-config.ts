80c824cdaa23f06a93261a03537e7455c654a3d4
alexiscirio209 <alexiscirio209@gmail.com>
2026-04-04 22:10:44 -0600

    Backend Medusa Babygoo

---
+++ medusa-config.ts
@@ -0,0 +1,33 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+export default defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+  modules: [
+    {
+      resolve: "@medusajs/payment",
+      options: {
+        providers: [
+          {
+            id: "stripe",
+            resolve: "@medusajs/payment-stripe",
+            options: {
+              apiKey: process.env.STRIPE_API_KEY,
+              webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+            },
+          },
+        ],
+      },
+    },
+  ],
+})
